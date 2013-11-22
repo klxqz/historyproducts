a09118475ab7fc479c1aaa243e60fbe9991d86ff
klxqz <klxqz@mail.ru>
2013-11-22 13:30:25 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,18 +3,17 @@
 return array(
     'name' => 'История просмотренных товаров',
     'description' => 'История просмотренных товаров',
-    'vendor'=>903438,
-    'version'=>'1.0.3',
-    'img'=>'img/historyproducts.png',
+    'vendor' => '985310',
+    'version' => '1.0.4',
+    'img' => 'img/historyproducts.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/historyproducts.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/historyproducts.png',
     ),
     'handlers' => array(
         'frontend_nav' => 'frontendNav',
         'frontend_product' => 'frontendProduct'
     ),
-
 );
 //EOF
